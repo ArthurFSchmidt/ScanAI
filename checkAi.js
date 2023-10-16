d9b2022e1bbac3aefd844c48982fb29b8cefa3cd
Arthur Ferreira Schmidt <arthur-fschmidt@educar.rs.gov.br>
2023-10-16 08:10:41 -0300

    Modificação do popup e botão fechar

--- checkAi.js
+++ checkAi.js
@@ -1,38 +1,43 @@
 console.log("checkai iniciado");
 
-
+// Cria o objeto que define o item para o contextMenu.
 let contextMenuItem = {
-    "id": "checkAI",
-    "title": "Procurar por conteúdo IA",
-    "contexts": ["selection"]
+  "id": "checkAI",
+  "title": "Procurar por conteúdo IA",
+  "contexts": ["selection"]
 }
+
+// Cria o item no contextMenu utilizando o objeto como base.
 chrome.contextMenus.create(contextMenuItem);
 
+// Adiciona um evento onClick ao item.
 chrome.contextMenus.onClicked.addListener(aiChecker);
 
-async function aiChecker(clickData){
+// Função assíncrona que será chamada quando o item for clicado.
+// Tem como objetivo se conectar com a API para fazer a verificação,
+// após isso, cria o content script na página e manda as informações como mensagem.
+async function aiChecker(clickData) {
 
-    const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
-    let resp = "Não é AI!";
+  // Faz query para encontrar a página ativa.
+  const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+  let resp = "Não é AI!";
+  console.log(clickData);
 
 
 
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      files: ["scripts/content.js"]
-    });
-    
-    
-    chrome.runtime.onMessage.addListener(
-      function(request, sender, sendResponse) {
-        if (request.message === "getScanResult")
-          sendResponse({message: {'text': clickData.selectionText, 'result': resp}});
+  // Cria o content script na página ativa.
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ["scripts/content.js"]
+  });
+
+  // Espera a mensagem do content script e retorna o texto verificado e o resultado.
+  chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+      if (request.message === "getScanResult"){
+        sendResponse({ message: { 'text': clickData.selectionText, 'result': resp } });
       }
-    );
-    
+    }
+  );
+
 }
-
-
-
-
-  
--- scripts/content.js
+++ scripts/content.js
@@ -1,47 +1,68 @@
-    let body = document.querySelector("body");
-    let div = document.createElement("div");
-    div.className = "scanAI-results"
-    body.appendChild(div);
-    
-    const host = document.querySelector('.scanAI-results');
-    let shadowRoot = host.attachShadow({mode: 'open'});
-    
-    div = document.createElement("div");
-    div.style = "width:300px; min-height:150px; background-color: #f7f7ff; border: 1px solid #688bff; position:fixed; z-index: 1000; right: 10px; top: 10px;"
-    
-    let h1 = document.createElement("h1");
-    h1.style = "text-align: center;"
-    h1.appendChild(document.createTextNode("Resultado:"));
-    div.appendChild(h1);
+criaPopup();
 
-    let hr = document.createElement("hr");
-    div.appendChild(hr);
-    
-    let pText = document.createElement("p");
-    pText.appendChild(document.createTextNode("Texto: "));
-    pText.style = "margin: 0 10px;";
-    pText.className = "scanAI-text";
 
-    div.appendChild(pText);
-    
-    let pResult = document.createElement("p");
-    pResult.style = "margin: 0 10px;";
-    pResult.className = "scanAI-result";
+function criaPopup(){
+  // Cria div vazia
+  let body = document.querySelector("body");
+  let div = document.createElement("div");
+  div.className = "scanAI-results"
+  body.appendChild(div);
 
-    div.appendChild(pResult);
+  // Insere uma shadowRoot na div, para que não herde nenhum css.
+  const host = document.querySelector('.scanAI-results');
+  let shadowRoot = host.attachShadow({ mode: 'open' });
 
-    hr = document.createElement("hr");
-    div.appendChild(hr);
-    
-    shadowRoot.appendChild(div);
-        
+  // Criação da div verdadeira que estará visível na página.
+  div = document.createElement("div");
+  div.classList.add("scanIA-popup-div");
+  div.style = "width:300px; min-height:150px; background-color: #f7f7ff; border: 1px solid #688bff; position:fixed; z-index: 100000; right: 10px; top: 10px;"
+
+  let h1 = document.createElement("h1");
+  h1.style = "text-align: center;"
+  h1.appendChild(document.createTextNode("Resultado:"));
+  div.appendChild(h1);
+
+  let hr = document.createElement("hr");
+  div.appendChild(hr);
+
+  let pText = document.createElement("p");
+  pText.appendChild(document.createTextNode("Texto: "));
+  pText.style = "margin: 0 10px; text-align: justify;";
+  pText.className = "scanAI-text";
+
+  div.appendChild(pText);
+
+  let pResult = document.createElement("p");
+  pResult.style = "margin: 10px 10px;";
+  pResult.className = "scanAI-result";
+
+  div.appendChild(pResult);
+
+  hr = document.createElement("hr");
+  div.appendChild(hr);
+
+  let btn = document.createElement("button");
+  btn.innerHTML = "Fechar";
+  div.appendChild(btn);
+  btn.onclick = fecharPopup
+
+  // Adiciona a div ao shadowRoot
+  shadowRoot.appendChild(div);
+  
+  // Função assíncrona para receber informações do serviceWorker. 
   (async () => {
-    const response = await chrome.runtime.sendMessage({message: "getScanResult"});
-    console.log(response);
-
+    // Manda mensagem para o serviceWorker e recebe as informações necessárias como resposta.
+    const response = await chrome.runtime.sendMessage({ message: "getScanResult"});
+  
     pText.appendChild(document.createTextNode(response.message.text));
     pResult.appendChild(document.createTextNode(response.message.result));
-    
-    console.log(response);
+  
+    timesActivated++;
   })();
+}
 
+function fecharPopup() {
+  let div = document.querySelector(".scanAI-results");
+  div.parentNode.removeChild(div);
+  console.log(div);
+}
